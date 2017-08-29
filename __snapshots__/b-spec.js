38f1f4b665c2c60e6f207d2dbf6ead97cb4f9603
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-29 09:19:16 -0400

    feat(text): for long multiline text print just before and after

--- __snapshots__/b-spec.js
+++ __snapshots__/b-spec.js
@@ -1,4 +1,17 @@
-exports['b-spec works too 1'] = "some value"
+exports['b-spec works too 1'] = `some value`
 
-exports['b-spec works 1'] = "value b"
+exports['b-spec works 1'] = `value b`
+
+exports['b-spec works with long text 1'] = `
+      line 1
+      line 2
+      line 3
+      line 4
+      line 5
+      line 6
+      line 7
+      line 8
+      line 9
+      line 10
+    `
 
--- test/two-specs/b-spec.js
+++ test/two-specs/b-spec.js
@@ -5,4 +5,20 @@ describe('b-spec', () => {
   it('works', () => {
     snapshot('value b')
   })
+
+  it('works with long text', () => {
+    const text = `
+      line 1
+      line 2
+      line 3
+      line 4
+      line 5
+      line 6
+      line 7
+      line 8
+      line 9
+      line 10
+    `
+    snapshot(text)
+  })
 })
