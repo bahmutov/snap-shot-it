e33712b996f1e354d0c12fb26ea3d470e964c706
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-19 00:40:53 -0400

    update specs

--- __snapshots__/snapshots.js
+++ __snapshots__/snapshots.js
@@ -231,5 +231,22 @@ module.exports = {
       }
     }
   },
+  "test/two-specs/a-spec.js": {
+    "a-spec": {
+      "works": {
+        "1": "value a"
+      }
+    }
+  },
+  "test/two-specs/b-spec.js": {
+    "b-spec": {
+      "works": {
+        "1": "value b"
+      },
+      "works with long text": {
+        "1": "\n      line 1\n      line 2\n      line 3\n      line 4\n      line 5\n      line 6\n      line 7\n      line 8\n      line 9\n      line 10\n    "
+      }
+    }
+  },
   "__version": "0.0.0-development"
 }
