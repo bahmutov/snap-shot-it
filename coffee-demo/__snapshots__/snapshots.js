709a49b1d8b046577839363f1de31702d73cc170
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-19 00:35:31 -0400

    run demos

---
+++ coffee-demo/__snapshots__/snapshots.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "spec.coffee": {
+    "CoffeeScript with snapshots": {
+      "works": {
+        "1": 42,
+        "2": "foo",
+        "3": {
+          "foo": "foo",
+          "bar": "bar"
+        },
+        "4": "a promise"
+      }
+    }
+  },
+  "__version": "0.0.0-development"
+}
