e46180cb6a0429a180bbeec55852347ec7d68962
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-19 00:06:11 -0400

    working against file system store

---
+++ __snapshots__/snapshots.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "a-spec": {
+    "works": "value a"
+  },
+  "b-spec": {
+    "works": "value b",
+    "works with long text": "\n      line 1\n      line 2\n      line 3\n      line 4\n      line 5\n      line 6\n      line 7\n      line 8\n      line 9\n      line 10\n    "
+  },
+  "__version": "0.0.0-development"
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 'use strict'
 
 const debug = require('debug')('snap-shot-it')
-const core = require('snap-shot-core')
+// const core = require('snap-shot-core')
+const { initStore } = require('snap-shot-store')
 const compare = require('snap-shot-compare')
 const { isDataDriven, dataDriven } = require('@bahmutov/data-driven')
 const { isNamedSnapshotArguments } = require('./named-snapshots')
 const R = require('ramda')
+const itsName = require('its-name')
+const la = require('lazy-ass')
+const is = require('check-more-types')
+const fs = require('fs')
+const path = require('path')
+const makeDir = require('make-dir')
+
+const version = require('../package.json').version
 
 debug('loading snap-shot-it')
 const EXTENSION = '.js'
+const SNAPSHOT_FOLDER = path.join(process.cwd(), '__snapshots__')
+const SNAPSHOT_FILENAME = path.join(SNAPSHOT_FOLDER, `snapshots${EXTENSION}`)
+
+let storeSnapshot
+
+global.before(function loadSnapshots() {
+  debug('loading snapshot store')
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(SNAPSHOT_FILENAME)) {
+      debug('loading snapshot store from file')
+      const store = require(SNAPSHOT_FILENAME)
+      delete store.__version
+      storeSnapshot = initStore(store)
+    } else {
+      debug('starting new snapshot store')
+      storeSnapshot = initStore()
+    }
+    resolve()
+  })
+})
 
 // all tests we have seen so we can prune later
 const seenSpecs = []
 function pruneSnapshots () {
   debug('pruning snapshots')
   debug(seenSpecs)
-  core.prune({ tests: seenSpecs, ext: EXTENSION })
+  // core.prune({ tests: seenSpecs, ext: EXTENSION })
   // eslint-disable-next-line immutable/no-mutation
   seenSpecs.length = 0
 }
@@ -51,12 +80,12 @@ function clearCurrentTest () {
   if (currentTest) {
     const fullTitle = getTestTitle(currentTest)
     debug('clearing current test "%s"', fullTitle)
-    core.restore(getTestInfo(currentTest))
+    // core.restore(getTestInfo(currentTest))
     currentTest = null
   }
 }
 
-global.beforeEach(function () {
+global.beforeEach(function setCurrentTest () {
   /* eslint-disable immutable/no-this */
   if (this.currentTest) {
     setTest(this.currentTest)
@@ -105,22 +134,31 @@ function snapshot (value) {
     update: Boolean(process.env.SNAPSHOT_UPDATE),
     ci: Boolean(process.env.CI)
   }
+  const name = itsName(currentTest)
+  la(is.strings(name), 'could not get name from current test', currentTest)
+
   const snap = {
-    what: value,
-    file: currentTest.file,
-    specName: savedTestTitle,
-    ext: EXTENSION,
-    compare,
+    value,
+    name,
     opts
   }
-  return core(snap)
+  // should we add test filename to the names?
+  // const snap = {
+  //   what: value,
+  //   file: currentTest.file,
+  //   specName: savedTestTitle,
+  //   ext: EXTENSION,
+  //   compare,
+  //   opts
+  // }
+  return storeSnapshot(snap)
 }
 
 /* eslint-disable immutable/no-mutation */
 module.exports = snapshot
 /* eslint-enable immutable/no-mutation */
 
-global.after(pruneSnapshots)
+// global.after(pruneSnapshots)
 
 function deleteFromCache () {
   // to work with transpiled code, need to force
@@ -129,3 +167,16 @@ function deleteFromCache () {
   delete require.cache[__filename]
 }
 global.after(deleteFromCache)
+
+function saveSnapshots () {
+  return makeDir(SNAPSHOT_FOLDER)
+    .then(() => {
+      debug('saving snapshots from version %s', version)
+      const snapshots = storeSnapshot()
+      snapshots.__version = version
+      const s = JSON.stringify(snapshots, null, 2)
+      const str = 'module.exports = ' + s + '\n'
+      fs.writeFileSync(SNAPSHOT_FILENAME, str)
+    })
+}
+global.after(saveSnapshots)
