1eab4943af51cd62522bdf11d4a44d51044d7cbf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-05-04 14:24:51 -0400

    update test

--- __snapshots__/aa-spec.js
+++ __snapshots__/aa-spec.js
@@ -1,3 +1,7 @@
-exports['a-spec works 1'] = `
+exports['a-spec works first 1'] = `
 value a
+`
+
+exports['a-spec works second 1'] = `
+value b
 `
--- test/failing-hook/aa-spec.js
+++ test/failing-hook/aa-spec.js
@@ -6,8 +6,12 @@ describe('a-spec', () => {
     // this hook fails on purpose
     // the failing hook should NOT trim the previously saved
     // snapshot from the test below
+    // throw new Error('beforeEach error!')
   })
-  it('works', () => {
+  it('works first', () => {
     snapshot('value a')
+  })
+  it('works second', () => {
+    snapshot('value b')
   })
 })
